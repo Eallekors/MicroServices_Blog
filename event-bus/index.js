f0c130abea9934080e8cbf474c780d29cf11f62c
Eallekors <Erkxalle@gmail.com>
2024-11-14 14:25:04 +0200

    Kubernetes setup

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,15 +12,15 @@ app.post('/events', (req, res) => {
 
     events.push(event)
 
-    axios.post('http://localhost:4000/events', event)
+    axios.post('http://posts-clusterip-srv:4000/events', event)
     .catch(err => console.error('Error posting to 4000:', err.message));
-axios.post('http://localhost:4001/events', event)
+ axios.post('http://localhost:4001/events', event)
     .catch(err => console.error('Error posting to 4001:', err.message));
 axios.post('http://localhost:4002/events', event)
     .catch(err => console.error('Error posting to 4002:', err.message));
 axios.post('http://localhost:4003/events', event)
     .catch(err => console.error('Error posting to 4003:', err.message));
-
+ 
 
     res.send({ status: 'OK' })
 })
